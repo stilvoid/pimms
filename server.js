587d6453d151887014f4a4b5733b876e8b82b803
Steve Engledow <steve@offend.me.uk>
2012-06-22 14:41:16 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,105 @@
+var fs = require("fs");
+var path = require("path");
+var spawn = require("child_process").spawn;
+
+var nosef = require("nosef");
+
+var dir = process.cwd();
+
+var media_player_command = "mplayer";
+var media_player_options = [];
+
+var current_player = null;
+
+var allowed_file_patterns = [
+    /.mp\w+$/i,
+    /.mov$/i,
+    /.avi$/i,
+    /.flv$/i,
+    /.wav$/i,
+    /.flac$/i
+];
+
+if(process.argv.length > 2) {
+    dir = process.argv[2];
+}
+
+console.log("Starting in", dir);
+
+function cd(request, response, params) {
+    var new_dir = path.join(dir, path.normalize(decodeURIComponent(params.get.path)));
+
+    var output = {
+        "files": [],
+        "folders": []
+    };
+
+    fs.readdir(new_dir, function(err, data) {
+        if(err) {
+            response.error("Could not cd to " + new_dir);
+        } else {
+            dir = new_dir;
+
+            data.forEach(function(entry) {
+                var matched;
+
+                if(!/^\./.test(entry)) {
+                    if(fs.statSync(path.join(dir, entry)).isDirectory()) {
+                        output.folders.push(entry);
+                    } else {
+                        matched = false;
+
+                        allowed_file_patterns.forEach(function(pattern) {
+                            if(pattern.test(entry)) {
+                                matched = true;
+                            }
+                        });
+
+                        if(matched) {
+                            output.files.push(entry);
+                        }
+                    }
+                }
+            });
+
+            output.folders.sort();
+            output.files.sort();
+
+            response.JSON(output);
+        }
+    });
+}
+
+function play(request, response, params) {
+    var file = path.join(dir, path.normalize(decodeURIComponent(params.get.path)));
+
+    if(current_player) {
+        current_player.kill();
+    }
+
+    fs.stat(file, function(err, data) {
+        if(err) {
+            response.error("Could not play " + file);
+        } else {
+            current_player = spawn(media_player_command, media_player_options.concat(file));
+
+            response.JSON(file);
+        }
+    });
+}
+
+var config = {
+    port: 8001,
+    host: "0.0.0.0",
+    middleware: function(request, response) {
+        console.log(request.url);
+    },
+    urls: [
+        ["/play/", play],
+        ["/cd/", cd],
+        ["/media/{{path}}", nosef.handlers.file("./media/", "path")],
+        ["/", nosef.handlers.file("./media/index.html")],
+    ]
+};
+
+nosef.server(config);
